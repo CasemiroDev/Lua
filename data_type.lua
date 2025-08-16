35c315d9d15eaa390dd751831a2312983e450a59
Gustavo Casemiro <gustavo.casemiro1@gmail.com>
2025-08-16 10:07:53 -0300

    commit

---
+++ data_type.lua
@@ -0,0 +1,11 @@
+--[[
+Tipos de dados:
+nil -- diferencia o valor de ter alguns dados ou nenhum dado(nulo) 
+boolean -- inclui o verdadeiro ou falso como valores
+number -- números reais como valores(ponto flutuante de precisão dupla)
+string -- matriz de caracteres
+function -- representa um método(também usado em C)
+userdata -- representa dados C arbitrários
+thread -- representa threads de execução independentes e é usado para implementar co-rotinas
+table -- representa matrizes comuns, implementa matrizes associativas, contendo qualquer valor (- nil)
+]]
